869fbb4bb8294486acd0a35d2cca2c09aa03297a
Vaideeswaran C <vaideeswaran@agentz.ai>
2020-07-25 16:17:57 +0530

    Addding validattion

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ const select = document.querySelector('#select')
 const form = document.querySelector('.form')
 const container1 = document.querySelector('#clientIdContainer')
 const container2 = document.querySelector('#clientSecretContainer')
-const container3 = document.querySelector('#redirectURLContainer')
 const container4 = document.querySelector('#refreshTokenContainer')
 const clientId = document.querySelector('#clientId')
 const clientSecret = document.querySelector('#clientSecret')
-const redirectURL = document.querySelector('#redirectURL')
 const refreshToken = document.querySelector('#refreshToken')
+const clientIdError = document.querySelector('#clientIdError')
+const clientSecretError = document.querySelector('#clientSecretError')
+const refreshTokenError = document.querySelector('#refreshTokenError')
+const selectError = document.querySelector('#selectError')
+
 
 const printTheOutput = event => {
     event.preventDefault();
     console.log('select', select.options[select.value].innerHTML)
     console.log('clientId', clientId.value)
     console.log('clientSecret', clientSecret.value)
-    console.log('redirectURL', redirectURL.value)
     console.log('refreshToken', refreshToken.value)
 }
 
@@ -24,20 +26,27 @@ const onSelect = event => {
     if(actionValue ===  '1') {
         container1.classList.remove('hide')
         container2.classList.remove('hide')
-        container3.classList.remove('hide')
         container4.classList.add('hide')
+        clientId.required = true
+        clientSecret.required = true
+        refreshToken.required = false
+
     }
     if(actionValue ===  '2') {
         container1.classList.remove('hide')
         container2.classList.remove('hide')
-        container3.classList.remove('hide')
         container4.classList.remove('hide')
+        clientId.required = true
+        clientSecret.required = true
+        refreshToken.required = true
     }
     if(actionValue ===  '3') {
         container1.classList.add('hide')
         container2.classList.add('hide')
-        container3.classList.add('hide')
         container4.classList.remove('hide')
+        clientId.required = false
+        clientSecret.required = false
+        refreshToken.required = true
     }
 }
 
